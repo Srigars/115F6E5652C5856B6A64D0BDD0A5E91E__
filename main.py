1147fabe3178a7f77ad18f610597d912c96d84fd
Srigars <144204926+Srigars@users.noreply.github.com>
2023-09-06 12:29:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#leap year
-def isleapyear(year):
-  if(year%4==0 and year%100!=0)or year%400==0:
-    return True
+# Implement a recursive function to calculate the factorial of a given number
+def fact_rec(n):
+  if n==0 or n==1:
+    return 1
   else:
-    return False
-year=int(input("enter a year:"))
-if isleapyear(year):
-   print('{} is a leap year.'.format(year))
-else:
-   print('{}is a not leapyear.'.format(year))
+     return n*fact_rec(n-1)
+number=int(input("Enter a value:"))
+res=fact_rec(number)
+print(" the factorial of {}is{}.".format(number,res))
+  
